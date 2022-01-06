4dc047443ceaf6dfc1ba83629e60e0f8f76a7039
piotrbuczek2 <97125424+piotrbuczek2@users.noreply.github.com>
2022-01-06 15:52:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
-terraform {
-  cloud {
-    organization = "4"
-
-    workspaces {
-      name = "df"
-    }
-  }
-}
 resource "random_string" "my_string" {
   length = var.my_length
   number = var.my_number
 }
-
-#terraform apply -var "my_length=33" -var "my_number=false"
-
